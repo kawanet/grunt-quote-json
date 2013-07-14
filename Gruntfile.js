6a7f7fc25fff63b2c19768810f6c87ec0552cb38
kawanet <u-suke@kawa.net>
2013-07-15 01:25:40 +0900

    quote root fields

---
+++ Gruntfile.js
@@ -0,0 +1,89 @@
+/*
+ * grunt-quote-json
+ * https://github.com/kawanet/grunt-quote-json
+ *
+ * Copyright (c) 2013 kawanet
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+          'Gruntfile.js',
+          'tasks/*.js',
+          '<%= nodeunit.tests %>',
+      ],
+      options: {
+        'node': true
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    copy: {
+      tests: {
+        src: 'test/defaults/test2.json',
+        dest: 'tmp/test2.json'
+      }
+    },
+
+    // Configuration to be run (and then tested).
+    quoteJson: {
+      test1: {
+        src: 'package.json',
+        dest: 'tmp/test1.json',
+        options: {
+          fields: {
+            name: 1,
+            description: 1
+          }
+        }
+      },
+      test2: {
+        src: 'test/sources/test2.json',
+        dest: 'tmp/test2.json',
+        options: {
+          fields: {
+            string: 1,
+            numeric: 1,
+            object: 1,
+            array: 1,
+            // "children.boy": 1,
+            // "children.baby": 1
+          }
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'copy', 'quoteJson', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ README.md
@@ -0,0 +1,53 @@
+# grunt-quote-json
+
+Grunt contirb plugin to quote a part of JSON to another
+
+## Usage
+
+This task quotes a part of the source JSON file into the destination JSON file.
+
+Load `grunt-quote-json` module and install `quoteJson` task at your `Gruntfile.js`.
+
+```js
+grunt.loadNpmTasks('grunt-quote-json');
+
+grunt.initConfig({
+  quoteJson: {
+    bower: {
+      src: 'package.json',
+      dest: 'bower.json',
+      options: {
+        fields: {
+          name: 1,
+          version: 1,
+          description: 1,
+          repository: 1
+        }
+      }
+    }
+  }
+})
+```
+
+In the case above, it copies `name`, `version`, `description`
+and `repository` fields from `package.json` file to `bower.json` file.
+You don't need care the consistency of version number etc.
+between those JSON files with the plugin.
+
+## INSTALLATION
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```sh
+npm install grunt-quote-json --save-dev
+```
+
+## LINKS
+
+### Sources
+
+https://github.com/kawanet/grunt-quote-json
+
+### Author
+
+https://github.com/kawanet
